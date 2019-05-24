6f1ea62c2dd6eaa0fa0a1a21c07660e96d348d4f
Rodney Osodo <blackd0t@protonmail.com>
2019-05-24 22:51:33 +0300

    opencv file for detection

--- faces.py
+++ faces.py
@@ -1,7 +1,18 @@
 import numpy as np
 import cv2
+import pickle
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 face_cascade = cv2.CascadeClassifier("Models/haarcascade_frontalface_alt2.xml")
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("Models/trainer.yml")
+
+labels = {}
+with open("Models/labels.pkl", "rb") as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
@@ -10,12 +21,19 @@ while True:
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 	for (x, y, w, h) in faces:
-		print(x, y, w, h)
+		#print(x, y, w, h)
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = frame[y:y+h, x:x+w]
 
-		#Recognize deep learnde model to predict
-
+		id_, conf = recognizer.predict(roi_gray)
+		if conf >= 45 and conf <= 85:
+			print(id_)
+			print(labels[id_])
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			name = labels[id_]
+			color = (255,255,255)
+			stroke = 2
+			cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
 		img_item = "my-image.png"
 		cv2.imwrite(img_item, roi_gray)
 
@@ -24,10 +42,7 @@ while True:
 		end_cord_x = x + w
 		end_cord_y = y + h
 		cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
-
-
 	cv2.imshow("frame", frame)
-
 	if cv2.waitKey(20) & 0xFF == ord("q"):
 		break
 		
