06febc619470a806aa51aff3845d9bdce4d6cf5e
Rodney Osodo <blackd0t@protonmail.com>
2019-05-31 22:32:23 +0300

    Refractoring by removing unnecessary files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Emojidetection
 Replacing your emotions or signs you do with emojis
+[![License MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/saschagrunert/webapp.rs/blob/master/LICENSE)]
--- app.py
+++ app.py
@@ -1,53 +1,21 @@
-from sys import stdout
-from makeup_artist import Makeup_artist
-import logging
 from flask import Flask, render_template, Response
-from flask_socketio import SocketIO
-from camera import Camera
-from utils import base64_to_pil_image, pil_image_to_base64
+from videostream import VideoStream
 
-
-app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(stdout))
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
-socketio = SocketIO(app)
-camera = Camera(Makeup_artist())
-
-
-@socketio.on('input image', namespace='/test')
-def test_message(input):
-    input = input.split(",")[1]
-    camera.enqueue_input(input)
-    #camera.enqueue_input(base64_to_pil_image(input))
-
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    app.logger.info("client connected")
-
+app = Flask(__name__, template_folder="Web/templates", static_folder='Web/static')
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
-
-def gen():
-    """Video streaming generator function."""
-
-    app.logger.info("starting to generate frames!")
+def gen(stream):
     while True:
-        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        frame = stream.detect_faces()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/video_feed')
+@app.route('/video')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return Response(gen(VideoStream()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(debug=True)
